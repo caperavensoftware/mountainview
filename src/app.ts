e7b3710a054932e9a31545f4655f06b9683ee6e7
caperaven@icloud.com <caperaven@icloud.com>
2016-08-13 23:42:42 +0200

    initial

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,8 @@ export class App {
     configureRouter(config, router) {
         config.title = 'Center for Biblical Studies';
         config.map([
-            {route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome/welcome',      nav: true, title: 'Welcome'},
+            {route: 'welcome', name: 'welcome',      moduleId: 'views/welcome/welcome',      nav: true, title: 'Welcome'},
+            {route: ['', 'construction'], name: 'construction',      moduleId: 'views/construction/construction',      nav: true, title: 'Construction'},
         ]);
 
         this.router = router;
