6201ef4bdf9cbb8ce3b92dd19d8b734f3c5bd296
caperaven@icloud.com <caperaven@icloud.com>
2016-09-03 10:39:27 +0200

    added worker to create ui

--- src/views/welcome/welcome.ts
+++ src/views/welcome/welcome.ts
@@ -1,24 +1,45 @@
 import {bindable} from 'aurelia-framework';
 
+const createMenuMessage = "create-menu";
+
+class WelcomeUIAdapter {
+    updateCourses(html) {
+        requestAnimationFrame(() => {
+            document.getElementById("courses").innerHTML = html;
+        });
+    }
+}
+
 export class Welcome {
     courses;
-    welcomeWorker
+    welcomeWorker;
+    uiAdapter;
 
     constructor() {
+        this.uiAdapter = new WelcomeUIAdapter();
+
         this.welcomeWorker = new Worker("./app/src/webworkers/welcome-worker.js");
-        this.welcomeWorker.onmessage = this.onWelcomeWorkerMessage;
-        this.loadCourses();
+        this.welcomeWorker.onmessage = this.onWelcomeWorkerMessage.bind(this);
     }
 
     onWelcomeWorkerMessage(event) {
-        console.log(event.data)
+        if (event.data.action === createMenuMessage) {
+            this.uiAdapter.updateCourses(event.data.result);
+        }
     }
 
     loadCourses() {
         const link = document.getElementById("coursesData");
         this.courses = JSON.parse((<any>link).import.querySelector("body").innerHTML);
 
-        this.welcomeWorker.postMessage("greet");
+        this.welcomeWorker.postMessage({
+            action: createMenuMessage,
+            data: this.courses
+        });
+    }
+
+    attached() {
+        this.loadCourses();
     }
 
     detached() {
@@ -28,5 +49,10 @@ export class Welcome {
         }
 
         this.courses = null;
+        this.uiAdapter = null;
+    }
+
+    itemSelected(event) {
+        console.log("item clicked");
     }
 }
--- src/webworkers/welcome-worker.ts
+++ src/webworkers/welcome-worker.ts
@@ -1,10 +1,19 @@
 declare function postMessage(response: any);
 
+const createMenuMessage = "create-menu";
+
 onmessage = function(e) {
-    console.log('Message received from main script');
-    
-    if (e.data === "greet") {
-        postMessage("hello world");
+    if (e.data.action === createMenuMessage) {
+        let result = "";
+
+        for(let course of e.data.data) {
+            result += `<li click.delegate="itemSelected($event)">${course.Name}</li>\n`
+        }
+
+        postMessage({
+            action: createMenuMessage,
+            result: result
+        });
     }
 };
 
