b295cb7352be11ffa70a4bf8bd85a339ccbb1624
caperaven@icloud.com <caperaven@icloud.com>
2016-09-06 16:45:46 +0200

    added worker to create ui

--- src/views/welcome/welcome.ts
+++ src/views/welcome/welcome.ts
@@ -2,40 +2,15 @@ import {bindable} from 'aurelia-framework';
 
 const createMenuMessage = "create-menu";
 
-class WelcomeUIAdapter {
-    updateCourses(html) {
-        requestAnimationFrame(() => {
-            document.getElementById("courses").innerHTML = html;
-        });
-    }
-}
-
 export class Welcome {
-    courses;
-    welcomeWorker;
-    uiAdapter;
+    @bindable courses;
 
     constructor() {
-        this.uiAdapter = new WelcomeUIAdapter();
-
-        this.welcomeWorker = new Worker("./app/src/webworkers/welcome-worker.js");
-        this.welcomeWorker.onmessage = this.onWelcomeWorkerMessage.bind(this);
-    }
-
-    onWelcomeWorkerMessage(event) {
-        if (event.data.action === createMenuMessage) {
-            this.uiAdapter.updateCourses(event.data.result);
-        }
     }
 
     loadCourses() {
         const link = document.getElementById("coursesData");
         this.courses = JSON.parse((<any>link).import.querySelector("body").innerHTML);
-
-        this.welcomeWorker.postMessage({
-            action: createMenuMessage,
-            data: this.courses
-        });
     }
 
     attached() {
@@ -43,13 +18,7 @@ export class Welcome {
     }
 
     detached() {
-        if (this.welcomeWorker) {
-            this.welcomeWorker.terminate();
-            this.welcomeWorker = null;
-        }
-
         this.courses = null;
-        this.uiAdapter = null;
     }
 
     itemSelected(event) {
