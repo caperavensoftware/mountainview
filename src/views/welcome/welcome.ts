dbf98fc352110a75399ed6eafad2d61b20b709a9
caperaven@icloud.com <caperaven@icloud.com>
2016-09-07 17:48:21 +0200

    added worker to create ui

--- src/views/welcome/welcome.ts
+++ src/views/welcome/welcome.ts
@@ -2,8 +2,16 @@ import {bindable} from 'aurelia-framework';
 
 const createMenuMessage = "create-menu";
 
+enum states {
+    showingCourses = 1,
+    showingSeminars = 2
+}
+
 export class Welcome {
     @bindable courses;
+    @bindable seminars;
+
+    currentState: states = states.showingCourses;
 
     constructor() {
     }
@@ -15,13 +23,41 @@ export class Welcome {
 
     attached() {
         this.loadCourses();
+        
     }
 
     detached() {
         this.courses = null;
     }
 
-    itemSelected(event) {
-        console.log("item clicked");
+    courseSelected(event) {
+        const id = event.target.getAttribute('data-id');
+        this.setSeminars(id);
+    }
+
+    seminarSelected(event) {
+        const id = event.target.getAttribute('data-id');
+        console.log(id);
+    }
+
+    setSeminars(id) {
+        const course = this.courses.find(function(course) {
+            if (course.Id == id) {
+                return course;
+            }
+        });
+        
+        this.seminars = course.Seminars;
+
+        this.setState(states.showingSeminars);
+    }
+
+    setState(state: states) {
+        if (state === states.showingSeminars) {
+            document.getElementById("seminars").style.transform = "translateX(0)";
+        }
+        else {
+            document.getElementById("seminars").style.transform = "translateX(381)";
+        }
     }
 }
